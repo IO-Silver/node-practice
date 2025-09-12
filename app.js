b6fd9c1f80c807362044174e33bab5b2f32167fb
Rio Silver <rjcsilver@gmail.com>
2025-09-12 16:25:59 -0400

    Added JSHint

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const EventEmitter = require("events");
-const emitter = new EventEmitter();
-const Logger = require("./logger");
-const logger = new Logger();
+const http = require("http");
 
-logger.on("log", (args) => {
-  console.log(`url: ${args.url}`);
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello World");
+    res.end();
+  }
+  if (req.url === "/api/courses") {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
+});
+server.on("connection", (socket) => {
+  console.log("New connection");
 });
 
-logger.log("message");
+server.listen(3000);
+
+console.log("Listening on port 3000...");

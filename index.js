df54408a42bdf2d51a3975ce390a7e9d461b967e
Rio Silver <rjcsilver@gmail.com>
2025-09-14 22:03:51 -0400

    Added put and delete

--- index.js
+++ index.js
@@ -30,16 +30,37 @@ app.post("/api/courses", (req, res) => {
   });
 
   const result = schema.validate(req.body);
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
-    return;
-  }
+  if (result.error) return res.status(400).send(result.error.details[0].message);
 
   const course = {
     id: courses.length + 1,
     name: req.body.name,
   };
   courses.push(course);
+  res.send(course);
+});
+
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send("The course with the given ID was not found.");
+
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send("The course with the given ID was not found.");
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
   res.send(course);
 });
 

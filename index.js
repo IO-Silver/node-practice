eadd37b3dde12d0a23fc904b59645c40b77743e2
Rio Silver <rjcsilver@gmail.com>
2025-09-15 17:06:47 -0400

    Including Mongoose and MongoDB

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const startupDebugger = require("debug")("app:startup");
+const dbDebugger = require("debug")("app:db");
 const express = require("express");
 const morgan = require("morgan");
 const config = require("config");
@@ -6,6 +7,7 @@ const helmet = require("helmet");
 const logger = require("./logger");
 const courses = require("./routes/courses");
 const home = require("./routes/home");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.json());
@@ -14,6 +16,37 @@ app.use(express.static("public"));
 app.use(helmet());
 app.use(morgan("tiny"));
 app.use(logger);
+
+mongoose
+  .connect("mongodb://localhost/mongo-exercises")
+  .then(() => dbDebugger("Connected to MongoDB..."))
+  .catch((err) => dbDebugger("Could not connect to MongoDB...", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  price: Number,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+
+const pageNumber = 2;
+const pageSize = 10;
+const Course = mongoose.model("Course", courseSchema);
+
+async function getCourses(id) {
+  const course = await Course.update({ _id: id });
+  if (!course) return;
+  course.set({
+    isPublished: true,
+    author: "Another Author",
+  });
+  const result = await course.save();
+  console.log(courses);
+}
+
+getCourses();
 
 app.set("view engine", "pug");
 app.set("views", "./views");

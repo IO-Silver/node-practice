b1e67d5b6f7d0b0b42a2a90c219598c9cc99c760
Rio Silver <rjcsilver@gmail.com>
2025-09-14 21:51:21 -0400

    Added joi and input validation

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const Joi = require("joi");
 const app = express();
+
+app.use(express.json());
 
 const courses = [
   { id: 1, name: "course1" },
@@ -12,12 +15,31 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
 
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course) return res.status(404).send("The course with the given ID was not found.");
+  res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  const result = schema.validate(req.body);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
   res.send(course);
 });
 

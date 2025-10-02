5a748317e42346ac79cdf33479c000e4a1015f75
Rio Silver <rjcsilver@gmail.com>
2025-10-01 23:15:14 -0400

    Added test content

--- index.test.js
+++ index.test.js
@@ -1,3 +1,33 @@
-test('hello world!', () => {
-	expect(1 + 1).toBe(2);
+const request = require("supertest");
+const express = require("express");
+const coursesRouter = require("./routes/courses");
+
+let app;
+
+beforeAll(() => {
+  app = express();
+  app.use(express.json());
+  app.use("/api/courses", coursesRouter);
+});
+
+test("hello world!", () => {
+  expect(1 + 1).toBe(2);
+});
+
+describe("GET /api/courses", () => {
+  it("should return an array of courses", async () => {
+    const res = await request(app).get("/api/courses");
+    expect(res.status).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+  });
+});
+
+describe("POST /api/courses", () => {
+  it("should create a new course and return it", async () => {
+    const newCourse = { name: "Test Course", author: "Test Author" };
+    const res = await request(app).post("/api/courses").send(newCourse);
+    expect(res.status).toBe(201);
+    expect(res.body).toHaveProperty("name", "Test Course");
+    expect(res.body).toHaveProperty("author", "Test Author");
+  });
 });
